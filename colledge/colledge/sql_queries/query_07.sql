a1f59091c6d160d271b3e75623f0d5048cd9ebc1
demagoit <33demagoit@gmail.com>
2024-03-14 18:42:57 +0200

    query_change

--- colledge/colledge/sql_queries/query_07.sql
+++ colledge/colledge/sql_queries/query_07.sql
@@ -2,4 +2,4 @@ SELECT g.name AS Team, sb.title AS Subject, m.mark AS Mark, s.name AS Name, s.su
 FROM marks AS m LEFT JOIN subjects AS sb ON m.subject_id = sb.id
 LEFT JOIN students AS s ON m.student_id = s.id
 LEFT JOIN groups AS g ON s.group_id = g.id
-WHERE Team = (?) AND Subject = (?);
+WHERE Team = "Group_1" AND Subject = "Math";
--- colledge/colledge/sql_queries/query_08.sql
+++ colledge/colledge/sql_queries/query_08.sql
@@ -2,4 +2,4 @@ SELECT p.name AS Name, p.surname AS Surname, sb.title AS Subject, AVG(m.mark) AS
 FROM marks AS m LEFT JOIN subjects AS sb ON m.subject_id = sb.id
 LEFT JOIN proffessors AS p ON sb.proffessor_id = p.id
 GROUP BY Name, Surname, Subject
-HAVING Surname = (?);
+HAVING Surname = "Martin";
